7f355a4f30173dc7922020f56870f50b8f899288
BartDrown <40639741+BartDrown@users.noreply.github.com>
2022-11-18 22:32:20 +0100

    feat: add turfs color ui, add saving system

--- src/client/client.ts
+++ src/client/client.ts
@@ -4,6 +4,8 @@ import { Biome } from './types/biome'
 import { GUI } from 'dat.gui'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import Stats from 'three/examples/jsm/libs/stats.module'
+import { Turf } from './types/turf'
+import { Vector3 } from 'three'
 import { renderPlane } from './render'
 
 const scene = new THREE.Scene()
@@ -18,9 +20,6 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 camera.position.z = 50
 camera.position.y = 50
 
-const geometry = new THREE.Vector3(50, 50, 50);
-const offset = new THREE.Vector3();
-const randomness = {seed: 0};
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -34,6 +33,16 @@ const stats = Stats()
 document.body.appendChild(stats.dom)
 
 const gui = new GUI()
+
+const geometry = new THREE.Vector3(50, 50, 50);
+const offset = new THREE.Vector3();
+const randomness = {seed: 0};
+
+gui.useLocalStorage = true;
+gui.remember(geometry)
+gui.remember(offset)
+gui.remember(randomness)
+
 const geometryFolder = gui.addFolder('Geometry')
 geometryFolder.add(geometry, 'x', 0, 1000)
 geometryFolder.add(geometry, 'y', 0, 1000)
@@ -53,24 +62,29 @@ noiseFolder.open()
 
 
 const biomesFolder = gui.addFolder('Biomes')
-const biomes: Biome[] =[]; 
+const biomesData = localStorage.getItem('biomesData');
+const biomes: Biome[] = biomesData ? JSON.parse(biomesData) as Biome[] : []; 
 
 biomesFolder.add({ AddBiome : ()=>{ 
-    const biomeName = `Biome ${biomes.length + 1}`;
-    biomes.push(new Biome(biomeName, 1000, -10, []));
-    biomesFolder.addFolder(biomeName);
+    addBiomeControls();
 }}, 'AddBiome').name('Add Biome')
 
 biomesFolder.add({ RemoveBiome : ()=>{ 
-    const biomeName = `Biome ${biomes.length}`;
-    if(biomes.length != 0){
-        biomes.pop();
-        biomesFolder.removeFolder(biomesFolder.__folders[biomeName])
+    const biomeToDelete = biomes.pop();
+    console.log(biomeToDelete)
+    if(biomeToDelete){
+        biomesFolder.removeFolder(biomesFolder.__folders[biomeToDelete.name])
     }
-
 }}, 'RemoveBiome').name('Remove Biome')
 
+biomes.forEach((biome, index)=>{
+    addBiomeControls(index, biome);
+})
 
+gui.add({ SaveAll : ()=>{ 
+    console.log(biomes)
+    localStorage.setItem('biomesData', JSON.stringify(biomes));
+}}, 'SaveAll').name('Save Biomes Data')
 
 function animate() {
     requestAnimationFrame(animate)
@@ -84,3 +98,52 @@ function render() {
 }
 
 animate()
+
+function addBiomeControls(indexParam?: number, biomeParam?: Biome){
+    const index = indexParam !== undefined ? indexParam : biomes.length
+    const biomeName = biomeParam?.name ? biomeParam.name : `Biome ${index + 1}`;
+    const biome = biomeParam ? biomeParam : new Biome(biomeName, 10, -10, []);
+    const newFolder = biomesFolder.addFolder(biomeName);
+
+    if(biomeParam === undefined){
+        biomes.push(biome);
+    }
+
+    newFolder.add(biome, 'maxTemperature', -1000, 1000);
+    newFolder.add(biome, 'minTemperature', -1000, 1000);
+    
+    
+    newFolder.add({ AddTurf : ()=>{ 
+        addTurfControls(newFolder, biome);
+    }}, 'AddTurf').name('Add Turf')
+
+
+    newFolder.add({ RemoveTurf : ()=>{ 
+        const turfName = `Turf ${biome.turfs.length}`;
+        if(biome.turfs.length != 0){
+            biome.turfs.pop();
+            newFolder.removeFolder(newFolder.__folders[turfName]);
+        }
+    }}, 'RemoveTurf').name('Remove Turf')
+
+    biome.turfs.forEach((turf, index)=>{
+        addTurfControls(newFolder, biome, index, turf);
+    })
+}
+
+function addTurfControls(folder: GUI, biome: Biome , indexParam?: number, turfParam?: Turf){
+    const index = indexParam !== undefined ? indexParam : biome.turfs.length;
+    const turfName = turfParam?.name ? turfParam.name : `Turf ${index + 1}`;
+    const turf = turfParam ? turfParam : new Turf(turfName, 10, -10, new THREE.Color(0xffffff), true);
+    const turfFolder = folder.addFolder(turfName);
+
+    if(turfParam === undefined){
+        biome.turfs.push(turf);
+    }
+
+    turfFolder.add(turf, 'maxElevation', -1000, 1000);
+    turfFolder.add(turf, 'minElevation', -1000, 1000);
+    turfFolder.addColor(turf, 'color');
+    turfFolder.add(turf, 'visible');
+
+}
--- src/client/types/turf.ts
+++ src/client/types/turf.ts
@@ -1,8 +1,18 @@
+import { Color } from "three";
+
 export class Turf{
     name: string = '';
-    color: string = 'ffffff';
+    color: Color;
     visible: boolean = true;
 
     maxElevation: number = Number.MAX_VALUE;
     minElevation: number = Number.MAX_VALUE;
+
+    constructor(name: string, maxElevation: number, minElevation: number, color: Color, visible: boolean ){
+        this.name = name;
+        this.maxElevation = maxElevation;
+        this.minElevation = minElevation;
+        this.color = color;
+        this.visible = visible;
+    }
 }
